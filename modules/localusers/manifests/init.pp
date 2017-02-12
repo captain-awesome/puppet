0014ed9bd6734d558b79360452f75e265ac2769f
captain-awesome <mr.captain.awesome@gmail.com>
2017-02-12 16:17:14 -0500

    edited localusers

--- modules/localusers/manifests/init.pp
+++ modules/localusers/manifests/init.pp
@@ -1,3 +1,10 @@
 class localusers {
-
+  user {'admin':
+    ensure      => present,
+    shell       => '/bin/bash',
+    home        => '/home/admin',
+    gid         => 'wheel',
+    managehome  => true,
+    password    => '$6$mZF8RayN$JX4C17AosL0HA1ikjx4Pt3SKiZqIoCdeq.IkD.Vax4.l/.LL7gLTe/FuNfflUft5kMHlyI6J7SRHakBgfdtkp1',
+  }
 }
