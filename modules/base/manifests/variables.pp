05a3fb0db9c436322a00a5debbbb6784fb840584
captain-awesome <mr.captain.awesome@gmail.com>
2017-02-18 20:46:08 +0000

    edit classes

--- modules/base/manifests/variables.pp
+++ modules/base/manifests/variables.pp
@@ -5,11 +5,14 @@ class base::variables {
   #  group     => 'root',
   #  mode      => '0644',
   #}
-  $localvar = 'local var'
-  $topscope = 'this is you new topscope variable within the variables class'
 
-  notify {"${topscope} is your topscope variable":}
-  notify {"${nodescope} is your nodescope variable":}
+  $localvar = 'local var'
+  $topscope = 'this is your new top scope variable within the variables class'
+  $nodescope = 'this is your new node scope variable with the variables class'
+
+  #notify {"${topscope} is your topscope variable":}
+  notify {"${::topscope} is your top scope variable":}
+  notify {"${nodescope} is your node scope variable":}
   notify {"${localvar} is your local var variable":}
   notify {"${::operatingsystem} is your operating system":}
 }
