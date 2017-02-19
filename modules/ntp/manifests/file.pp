1a194991f2794115910574496d1ae7d17060b991
captain-awesome <mr.captain.awesome@gmail.com>
2017-02-19 13:10:20 -0500

    ntp file template

---
+++ modules/ntp/manifests/file.pp
@@ -0,0 +1,14 @@
+class ntp::file {
+
+  case  $::operatingsystem {
+    'ubuntu': {$template = 'ntp_unbuntu.conf'}
+    'centos': {$template = 'ntp_centos.conf'}
+    default:  {fail('OS $::operatingsystem is not currently supported')}
+  }
+  
+  file {'/etc/ntp.conf':
+    ensure  => file,
+    require => Package['ntp'],
+    content => template ("ntp/${template}.erb"),
+  }
+}
