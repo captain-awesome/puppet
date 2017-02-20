336316c036d6650a815a22b54988d6e6313940cf
captain-awesome <mr.captain.awesome@gmail.com>
2017-02-19 22:10:49 -0500

    ruby fix

--- modules/ntp/manifests/params.pp
+++ modules/ntp/manifests/params.pp
@@ -4,10 +4,10 @@ class ntp::params {
     'ubuntu': {
       $template = 'ntp_ubuntu.conf'
       $service_name = 'ntp'
-      $default_servers = ["0.ubuntu.pool.ntp.org iburst",
-                          "1.ubuntu.pool.ntp.org iburst",
-                          "2.ubuntu.pool.ntp.org iburst",
-                          "3.ubuntu.pool.ntp.org iburst",]
+      $default_servers = ["0.ubuntu.pool.ntp.org",
+                          "1.ubuntu.pool.ntp.org",
+                          "2.ubuntu.pool.ntp.org",
+                          "3.ubuntu.pool.ntp.org",]
     }
     'centos': {
       $template = 'ntp_centos.conf'
