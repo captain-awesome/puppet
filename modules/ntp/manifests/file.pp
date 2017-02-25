f23e6cc70fb82a70f7273ab6b7ec8cb1cbec383d
captain-awesome <mr.captain.awesome@gmail.com>
2017-02-25 03:09:22 -0500

    added class to node def in site.pp

--- modules/ntp/manifests/file.pp
+++ modules/ntp/manifests/file.pp
@@ -1,11 +1,12 @@
-class ntp::file {
+class ntp::file ($template = $ntp::params::template, $servers = $ntp::params::default_servers) inherits ntp{
 
+  notify {"tjs template variable is now ${template}":}
   $template = $ntp::params::template
   $servers = $ntp::params::default_servers
 
   file {'/etc/ntp.conf':
     ensure  => file,
     require => Package['ntp'],
-    content => template ("ntp/${template}.erb"),
+    #content => template ("ntp/${template}.erb"),
   }
 }
--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -1,10 +1,14 @@
 class ntp ($package) inherits ntp::params {
+  #could also be set as class ntp ($package = $ntp::params::package_name)
+  #but the params class was defined in the node def in site.pp
 
   package {'ntp':
     ensure => present,
     name   => $package,
   }
   #include ntp::params (no longer necessary due to inheritance of params class)
-  include ntp::file
+
+  #include ntp::file (no longer necesary due to class below)
+  class {'ntp::file': template => 'hello',}
   include ntp::service
 }
