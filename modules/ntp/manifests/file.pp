9ced058e016ccb0b3ebf10e3f8d4b4753ac4305f
captain-awesome <mr.captain.awesome@gmail.com>
2017-02-25 03:16:01 -0500

    added class to node def in site.pp

--- modules/ntp/manifests/file.pp
+++ modules/ntp/manifests/file.pp
@@ -2,7 +2,7 @@ class ntp::file ($template = $ntp::params::template, $servers = $ntp::params::de
 
   notify {"this is a template variable is now ${template}":}
   # $template = $ntp::params::template (if enabled, will error and produce cannot reassign error)
-  $servers = $ntp::params::default_servers
+  # $servers = $ntp::params::default_servers (if enabled, will error and produce cannot reassign error)
 
   file {'/etc/ntp.conf':
     ensure  => file,
