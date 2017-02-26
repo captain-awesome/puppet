7af40ee3f29a692a4d297052f32ddbdc329c80b8
captain-awesome <mr.captain.awesome@gmail.com>
2017-02-25 21:08:01 -0500

    site.pp pkg variable

--- manifests/site.pp
+++ manifests/site.pp
@@ -48,6 +48,7 @@ node default {
 
 node 'rabelard1.mylabserver.com' {
   include pe_repo::platform::ubuntu_1404_amd64
+  $package = 'node scope variable',
   class {'ntp': package => 'ntp',}
   include ntp::service
 }
