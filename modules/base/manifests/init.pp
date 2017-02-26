bd8fc8966bc0fc1b1e06546b8c03b47c2b572717
captain-awesome <mr.captain.awesome@gmail.com>
2017-02-26 10:19:42 -0500

    small fixes

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,20 +1,24 @@
 class base {
-  $dnsutils = $osfamily ? {
+
+  $package_bundle = ['tree', $dnsutils]
+
+  $dnsutils = $::osfamily ? {
     'RedHat'  => 'bind-utils',
     'Debian'  => 'dnsutils',
   }
 
-  $systemupdate = $osfamily ?{
+  $systemupdate = $::osfamily ?{
     'RedHat'  => '/usr/bin/yum update -y',
     'Debian'  => '/usr/bin/apt-get upgrade -y',
   }
-  package {['tree', $dnsutils]:
+
+  package {$package_bundle:
     ensure => present,
   }
 
   schedule { 'system-daily':
-    period  => daily,
-    range => '00:00 = :01-00',
+    period  => 'daily',
+    range   => '00:00 = 01:00',
   }
 
   exec { $systemupdate:
