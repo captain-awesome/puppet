d4fb5e1f5f026769d3e6b317d8152b3627deb289
captain-awesome <mr.captain.awesome@gmail.com>
2017-02-26 19:48:56 -0500

    hiera

--- modules/hierademo/manifests/init.pp
+++ modules/hierademo/manifests/init.pp
@@ -1,7 +1,7 @@
 # == Class: hierademo
 #
 class hierademo ($domain_name = 'default', $api_key = 'default_api_key'){
-  $domainname = hiera ('domain_name')
+  $domainname = hiera_array ('domain_name')
   notify { "the hiera domain name value is: ${domainname}": }
   notify { "the domain name value is: ${domain_name}": }
   notify { "the api key value is: ${api_key}": }
