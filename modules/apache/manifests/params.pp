57fdb770e5b88ff315efba586609d654c1c6ea4d
captain-awesome <mr.captain.awesome@gmail.com>
2017-03-04 13:05:27 -0500

    apache class

--- modules/apache/manifests/params.pp
+++ modules/apache/manifests/params.pp
@@ -7,20 +7,20 @@ class apache::params {
     $servername = $::hostname
   }
   $document_root = '/var/www/websites'
-  $log_dir = '.var/log/apache'
+  $log_dir = '/var/log/apache'
 
   case $::osfamily {
     'RedHat': {
       $package_name = 'httpd'
       $service_name = 'httpd'
       $conf_dir     = '/etc/httpd/conf'
-      $vhost_dir    = 'etc/httpd/conf.d'
+      $vhost_dir    = '/etc/httpd/conf.d'
     }
     'Debian': {
       $package_name = 'apache2'
       $service_name = 'apache2'
       $conf_dir     = '/etc/apache2/'
-      $vhost_dir    = 'etc/apache2/sites-enabled'
+      $vhost_dir    = '/etc/apache2/sites-enabled'
     }
     default: {
         fail('This operating system is not supported')
