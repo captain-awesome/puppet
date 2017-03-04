96a62d90ebe90b360a99f5d79556b7d77808f1b8
captain-awesome <mr.captain.awesome@gmail.com>
2017-03-04 10:19:51 -0500

    apache class

--- modules/apache/manifests/service.pp
+++ modules/apache/manifests/service.pp
@@ -2,9 +2,9 @@
 #
 class apache::service ($service_name = $apache::params::service_name) {
   service {'apache':
-    ensure => running,
-    name   => $service_name,
-    enable => running,
+    ensure  => running,
+    name    => $service_name,
+    enable  => running,
     require => Package['apache'],
   }
 }
